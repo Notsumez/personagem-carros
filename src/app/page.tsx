eef38190f216535feb80da9ea26577cdf70a1f45
Notsumez <adahorapirola@gmail.com>
2024-10-24 22:05:55 -0300

    Front end terminado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,28 +3,39 @@ import CapaCarros from '/public/capacarros.jpg';
 
 export default function Home() {
   return (
-    <div className="container">
-      <h2 className="">Descubra qual personagem do filme Carros você é!</h2>
-      <Image 
-        src={CapaCarros}
-        alt="Capa filme Carros"
-        className="w-72"
-      />
-      <form>
-        <div>
-          <label>Digite aqui o número do seu cartão de crédito.</label>
-          <input type="number" required />
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="p-8 rounded-lg shadow-lg bg-white">
+        <div className="flex flex-col items-center justify-center">
+          <h2 className="text-2xl font-bold mb-4 text-center">Descubra qual personagem do filme Carros você é!</h2>
+          <Image 
+            src={CapaCarros}
+            alt="Capa filme Carros"
+            className="w-72"
+            />
         </div>
-        <div>
-          <label>Digite aqui a data de validade do seu cartão de crédito.</label>
-          <input type="number" placeholder="MM/AAAA" maxLength={7} required/>
-        </div>
-        <div>
-          <label>Digite aqui o código de segurança do seu cartão de crédito.</label>
-          <input type="number" placeholder="XXX" maxLength={3} required/>
-        </div>
-        <button type="submit">Descobrir!</button>
-      </form>
+        <br />
+        <form>
+          <div>
+            <label className="block mb-2 font-medium">Digite aqui o número do seu cartão de crédito.</label>
+            <input type="number" placeholder="XXXX XXXX XXXX XXXX" className="w-full p-2 border border-gray-300 rounded-md" maxLength={19} required />
+          </div>
+          <br />
+          <div>
+            <label className="block mb-2 font-medium">Digite aqui a data de validade do seu cartão de crédito.</label>
+            <input type="number" placeholder="MM/AAAA" className="w-full p-2 border border-gray-300 rounded-md" maxLength={7} required/>
+          </div>
+          <br />
+          <div>
+            <label className="block mb-2 font-medium">Digite aqui o código de segurança do seu cartão de crédito.</label>
+            <input type="number" placeholder="XXX" className="w-full p-2 border border-gray-300 rounded-md" maxLength={3} required/>
+          </div>
+          <br />
+          <div className="flex items-center justify-center">
+            <button type="submit" className="font-medium hover:font-bold px-4 py-2 hover:px-5 border-black border-2 rounded transition">Descobrir!</button>
+          </div>
+          <br />
+        </form>
+      </div>
     </div>
   );
 }
